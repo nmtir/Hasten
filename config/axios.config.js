41b1924b371bbc9a8c31a3f091b645d856a1e7e9
ndmtir <ndmtir@gmail.com>
2025-01-26 18:25:47 +0100

    c

--- config/axios.config.js
+++ config/axios.config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { revalidatePath } from 'next/cache';
 
-const baseURL = process.env.NEXT_PUBLIC_SITE_URL + '/api';
+const baseURL = process.env.NEXT_PUBLIC_SITE_URL + '/api/v1';
 
 export const api = axios.create({
   baseURL,
